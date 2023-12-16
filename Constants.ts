19d56cfb74589741e45484e1d68aa4e286c23f88
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-12-16 11:20:35 +0300

    add constants

---
+++ Constants.ts
@@ -0,0 +1,11 @@
+const DELAY_BOIL_1L: number = 3 // 3 secondes pour 1 litre
+const DELAY_BOIL_2L: number = 5 // 5 secondes pour 2 litres
+const DELAY_COOK_WHITE_RICE: number = 7 // 7 secondes pour le riz blanc
+const DELAY_COOK_BROWN_RICE: number = 10 // 10 secondes pour le riz brun
+
+export {
+  DELAY_BOIL_1L,
+  DELAY_BOIL_2L,
+  DELAY_COOK_WHITE_RICE,
+  DELAY_COOK_BROWN_RICE
+}
