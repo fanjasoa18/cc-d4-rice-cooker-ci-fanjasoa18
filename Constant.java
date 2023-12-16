66164fdbcea1cd7b5de451dbd2f15db197685a80
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-12-16 14:43:12 +0300

    add constants

---
+++ Constant.java
@@ -0,0 +1,8 @@
+public class Constant {
+    public static final int DELAY_BOIL_1L = 3;  // 3 secondes pour 1 litre
+    public static final int DELAY_BOIL_2L = 5;  // 5 secondes pour 2 litres
+    public static final int DELAY_COOK_WHITE_RICE = 7;  // 7 secondes pour le riz blanc
+
+    // 10 secondes pour le riz brun
+    public static final int DELAY_COOK_BROWN_RICE = 10;
+}
